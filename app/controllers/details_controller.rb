605fce00dae6d01728d5d2c1211b07ce7b174cda
fumiblog <fumihisa.yano@gmail.com>
2020-12-26 14:37:00 +0000

    [imput edit] 2-2

--- app/controllers/details_controller.rb
+++ app/controllers/details_controller.rb
@@ -8,19 +8,27 @@ class DetailsController < ApplicationController
     def create
         @detail = Detail.new(detail_params)
         @detail.save
-        redirect_to 
+        redirect_to details_path
     end
     
     def show
     end
     
     def edit
+        @detail = Detail.find(params[:id])
+        @genres = Genre.all
     end
     
     def update
+        @detail = Detail.find(params[:id])
+        @detail.update(detail_params)
+        redirect_to details_path
     end
     
     def destroy
+        @detail = Detail.find(params[:id])
+        @detail.destroy
+        redirect_to details_path
     end
     
     private
